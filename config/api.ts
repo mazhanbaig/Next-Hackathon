282110c4df7e74276d5b60dc9fca09656a1c6dfb
mazhanbaig <mazhanbaig44@gmail.com>
2026-02-28 15:40:48 +0500

    type err

--- config/api.ts
+++ config/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let token=localStorage.getItem("token")
+let token:any=localStorage.getItem("token")
 const api = axios.create({
   baseURL: "http://localhost:5000",
   headers: {
